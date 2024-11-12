48d2c04c5619adde28b842a618c3e3f936aae591
jinvicky <jinvicky17@gmail.com>
2024-11-12 14:39:44 +0900

    feat: lcs 세 문자열을 같게 만드는 예제 풀이 추가, dp 기본 경로 최대 경우의 수 구하는 예제 풀이 추가

---
+++ src/main/java/dp/leetCode/UniquePaths.java
@@ -0,0 +1,46 @@
+package dp.leetCode;
+
+
+/**
+ * LeetCode 62. Unique Paths
+ *
+ * [소요시간]
+ * 15분 내로 정답
+ * [오래 걸린 부분]
+ * i와 j가 1일 때 continue; 해야 하는데 이 부분 조금 걸렸다.
+ *
+ * [풀이]
+ * 나동빈 코테 책 기본 dp 예제 공식이다.
+ * 오른쪽 또는 아래로만 이동할 수 있다고 하면 m과 n이 주어졌을 때 [m+1][n+1] 배열을 만든다.
+ * 각 칸은 dp의 본인 왼쪽과 본인 위쪽의 경우의 수의 SUM이다.
+ * 계속 dp에서 참조해서 재사용함으로써 O(m*n)의 시간복잡도를 가진다.
+ */
+public class UniquePaths {
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(i == 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+                if (j== 1) {
+                    dp[i][j] = 1;
+                    continue;
+                }
+
+                int fromTop = dp[i-1][j];
+                int fromLeft = dp[i][j-1];
+                dp[i][j] = fromLeft + fromTop;
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        UniquePaths uniquePaths = new UniquePaths();
+        System.out.println(uniquePaths.uniquePaths(3, 2));
+    }
+}
---
+++ src/main/java/lcs/leetCode/MakeThreeStringsEqual.java
@@ -0,0 +1,52 @@
+package lcs.leetCode;
+
+
+/**
+ * LeetCode 1897. Redistribute Characters to Make All Strings Equal
+ * <p>
+ * [난이도] - EASY
+ * <p>
+ * [회고]
+ * slice 계산이 어려웠다.
+ * 중간에 IS_DIFF의 경우 바로 break;를 했어야 했는데 그러지 못해서 마지막 테스트 케이스에서 에러 해결하느라 오래 걸렸다.
+ */
+public class MakeThreeStringsEqual {
+
+    private boolean isSame(char ch1, char ch2) {
+        return ch1 == ch2;
+    }
+
+    public int findMinimumOperations(String s1, String s2, String s3) {
+        int minLength = Math.min(s1.length(), Math.min(s2.length(), s3.length()));
+
+        int slice = -1;
+        for (int i = 0; i < minLength; i++) {
+            final boolean IS_DIFF = !(isSame(s1.charAt(i), s2.charAt(i)) && isSame(s2.charAt(i), s3.charAt(i)));
+
+            if (IS_DIFF) {
+                if (i == 0) return -1;
+
+                if (i <= minLength - 1) {
+                    slice = i;
+                    break;
+                } else {
+                    slice = minLength;
+                }
+            }
+        }
+        if (slice == -1) {
+            slice = minLength;
+        }
+        // 각 문자열의 길이에서 slice 포인터를 뺀 다음에 다 더하면 총 지워야 할 연산의 count가 나온다.
+        return (s1.length() - slice) + (s2.length() - slice) + (s3.length() - slice);
+    }
+
+    public static void main(String[] args) {
+        MakeThreeStringsEqual makeThreeStringsEqual = new MakeThreeStringsEqual();
+        System.out.println(makeThreeStringsEqual.findMinimumOperations("abc", "abb", "ab"));
+        System.out.println(makeThreeStringsEqual.findMinimumOperations("dac", "bac", "cac"));
+        System.out.println(makeThreeStringsEqual.findMinimumOperations("oby", "obz", "obf"));
+        System.out.println(makeThreeStringsEqual.findMinimumOperations("kui", "m", "v"));
+        System.out.println(makeThreeStringsEqual.findMinimumOperations("aca", "abcc", "accba"));
+    }
+}
